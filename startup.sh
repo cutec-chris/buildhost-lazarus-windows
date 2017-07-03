f14a5349706a0e30173242845d9efb2648cdb0f4
chris <christian@ullihome.de>
2017-07-03 07:09:47 +0000

    'startup.sh' ändern

--- startup.sh
+++ startup.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 /usr/bin/Xvfb :0 -screen 0 1024x768x24 > /dev/null &
 sleep 1
-/usr/bin/x11vnc &
+/usr/bin/x11vnc -q &
 sleep 1
-/root/novnc/utils/launch.sh --vnc localhost:5900 --listen 8080 > /dev/null &
+/root/novnc/utils/launch.sh --vnc localhost:5900 --listen 8080 &
 sleep 1
 xterm &
 #bash -c $1 $2 $3
 wine $1 $2 $3 $4 $5 $6
-killall xterm
-killall x11vnc
-killall Xvfb
